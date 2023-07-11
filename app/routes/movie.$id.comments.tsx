ff7484d47be246f9e331bc7ee24feab41a0f3499
edwinseptiana <edwin.septiana2@gmail.com>
2023-07-11 16:26:50 +0700

    tambah tombol delete comment

--- app/routes/movie.$id.comments.tsx
+++ app/routes/movie.$id.comments.tsx
@@ -1,10 +1,12 @@
 import { type LoaderArgs, json, type ActionArgs } from "@remix-run/node";
 import {
   Form,
+  useActionData,
   useLoaderData,
   useNavigation,
   useParams,
 } from "@remix-run/react";
+import { useEffect, useRef } from "react";
 import { db } from "~/utils/db.server";
 
 export async function loader({ params }: LoaderArgs) {
@@ -21,33 +23,61 @@ export async function loader({ params }: LoaderArgs) {
 
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
-  const data = await db.comment.create({
-    data: {
-      message: formData.get("comment") as String,
-      MovieId: formData.get("id") as String,
-    },
-  });
-  return json({ data });
+  const { _action } = Object.fromEntries(formData);
+
+  if (_action === "create") {
+    const data = await db.comment.create({
+      data: {
+        message: formData.get("comment") as String,
+        MovieId: formData.get("id") as String,
+      },
+    });
+    return json({ data });
+  }
+
+  if (_action === "delete") {
+    const data = await db.comment.delete({
+      where: { id: formData.get("postId") as String },
+    });
+    return json({ data });
+  }
 }
+
+const textareaClassName = `text-gray-600`;
 
 export default function Comments() {
   const { id } = useParams();
   const { data } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+
+  const isAdding = navigation.state === "submitting";
+
+  let formRef = useRef();
+  let commentRef = useRef();
+
+  useEffect(() => {
+    if (!isAdding) {
+      formRef.current?.reset();
+      commentRef.current?.focus();
+    }
+  }, [isAdding]);
+
   return (
     <div className=" rounded-lg border p-3">
       <h1 className=" text-xl font-semibold mb-5 text-orange-500">
         Your Opinion :
       </h1>
       <div>
-        <Form method="POST">
+        <Form method="post" ref={formRef}>
           <textarea
             name="comment"
             className=" w-full border border-orange-500 rounded-lg p-2 focus:ring-orange-600 focus:border-orange-600 focus:outline-orange-600"
             placeholder="Write your comment here..."
-          ></textarea>
+            ref={commentRef}
+          />
+
           <input type="hidden" name="id" value={id} />
-          {navigation.state === "submitting" ? (
+          {navigation?.state === "submitting" ? (
             <button
               type="button"
               disabled
@@ -59,17 +89,38 @@ export default function Comments() {
             <button
               type="submit"
               className=" bg-orange-500 px-4 py-2 rounded-lg text-white"
+              name="_action"
+              value="create"
             >
               Add Comment
             </button>
           )}
         </Form>
         <div className="mt-5 flex-col gap-y-3">
-          {data.map((post) => (
-            <div key={post.id}>
-              <p>{post.message}</p>
-            </div>
-          ))}
+          <ul className="list-disc space-y-2 pl-4 text-sm">
+            {data.map((post) => (
+              <li key={post.id} className={`${textareaClassName} font-mono`}>
+                <span className="text-gray-600">
+                  {post.message}{" "}
+                  <Form
+                    method="post"
+                    name="delete"
+                    style={{ display: "inline" }}
+                  >
+                    <input type="hidden" name="postId" value={post.id} />
+                    <button
+                      type="submit"
+                      name="_action"
+                      value="delete"
+                      className="bg-orange-600 h-5 w-5 text-white rounded-full"
+                    >
+                      x
+                    </button>
+                  </Form>
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
